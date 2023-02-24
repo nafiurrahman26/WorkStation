c733f6bbfa4a5cda29ad44fd1c8dbe4f6ffa9a37
nafiurrahman26 <nafiurrahmansabbir@gmail.com>
2023-02-25 00:41:37 +0600

    third commit

--- folder/C program/sum.c
+++ folder/C program/sum.c
@@ -1,7 +1,8 @@
 #include<stdio.h>
 int main(){
     int x,y,sum;
+    printf("Enter two numbrt: ");
     scanf("%d%d",&x,&y);
     sum=x+y;
-    printf("The sum is: %d",sum);
+    printf("The sum is: %d\n",sum);
 }
